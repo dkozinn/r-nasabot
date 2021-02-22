1ea7b5be4eba007515474d119b5739de2de39057
dkozinn <david@kozinn.com>
2021-02-22 09:22:54 -0500

    Use constant for flair ID

--- nasabot.py
+++ nasabot.py
@@ -11,6 +11,7 @@ REPLY_TEMPLATE= ("If you're visiting here perhaps for the first time from /r/all
                 "welcome to /r/nasa! Please take a moment to "
                 "[read our welcome post](https://www.reddit.com/r/nasa/comments/l43hoq/welcome_to_rnasa_please_read_this_post_for/)"
                 " before posting, and we hope you'll stick around for a while.")
+FLAIR_TEMPLATE_ID="b37f60b8-74ac-11eb-9178-0e509773c193"
 
 processed=[]        #Remember which submissions have already been processed
 
@@ -35,7 +36,7 @@ def main():
 def process_submission(submission):
     if submission.subreddit == SUB:
         logging.info("Replying in /r/"+str(submission.subreddit)+" to "+submission.title+" from "+str(submission.author)+" with ID "+submission.id)
-        submission.mod.flair(flair_template_id="b37f60b8-74ac-11eb-9178-0e509773c193")
+        submission.mod.flair(flair_template_id=FLAIR_TEMPLATE_ID)
         comment=submission.reply(REPLY_TEMPLATE)
         comment.mod.distinguish(how="yes",sticky=True)
 # TODO Store submissions that have already been processed -- probably use a list
