bfd50dde411a4c3245b08f890e9e0cc3a3edf8c0
dkozinn <david@kozinn.com>
2021-02-25 21:35:55 -0500

    Cleanup flow

--- nasabot.py
+++ nasabot.py
@@ -7,13 +7,13 @@ import logging
 
 import praw
 
-SUB="nasa"
+SUB="TOY"
 REPLY_TEMPLATE= ("If you're visiting here perhaps for the first time from /r/all, "
                 "welcome to /r/nasa! Please take a moment to "
                 "[read our welcome post](https://www.reddit.com/r/nasa/comments/l43hoq/welcome_to_rnasa_please_read_this_post_for/)"
                 " before posting, and we hope you'll stick around for a while.")
-FLAIR_TEMPLATE_ID="7216c708-7c40-11e4-b13d-12313d052165"
-
+#FLAIR_TEMPLATE_ID="7216c708-7c40-11e4-b13d-12313d052165"
+FLAIR_TEMPLATE_ID="b37f60b8-74ac-11eb-9178-0e509773c193"
 def main():
     reddit = praw.Reddit("nasabot")
     if strtobool(reddit.config.custom["app_debugging"]):
@@ -27,19 +27,21 @@ def main():
 
 # Iterate through submissions, process if it's the right subreddit and it either has no flair or it has flair not matching the template
 
-    for submission in reddit.subreddit('all').hot(limit=250):
+    for submission in reddit.subreddit('TOY').hot(limit=250):
         logging.info("Post in /r/"+str(submission.subreddit)+":"+submission.title+"/"+submission.id)
         if (submission.subreddit == SUB 
-                and ((not hasattr(submission,'link_flair_template_id') 
-                or  (hasattr(submission,'link_flair_template_id') 
-                    and submission.link_flair_template_id != FLAIR_TEMPLATE_ID)))):
-            process_submission(submission)
+                and getattr(submission,'link_flair_template_id',"NONE") != FLAIR_TEMPLATE_ID):
+             process_submission(submission)
 
 def process_submission(submission):
     logging.info("Replying in /r/"+str(submission.subreddit)+":"+submission.title+"/"+str(submission.author)+"/"+submission.id)
-    submission.mod.flair(flair_template_id=FLAIR_TEMPLATE_ID,text="/r/all")
-    comment=submission.reply(REPLY_TEMPLATE)
-    comment.mod.distinguish(how="yes",sticky=True)
+    try:
+        comment=submission.reply(REPLY_TEMPLATE)
+        comment.mod.distinguish(how="yes",sticky=True)
+        submission.mod.flair(flair_template_id=FLAIR_TEMPLATE_ID,text="/r/all")
+    except praw.exceptions.PRAWException as e:
+        logging.warning("Exception \"%s\" for id %s with title %s",e,submission.id,submission.title)
+
 
 if __name__ == "__main__":
     main()
