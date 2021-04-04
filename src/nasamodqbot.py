5075b3829f8b7381d02d1fdc1f4ec1d0bd48ff30
david@kozinn.com <david@kozinn.com>
2021-04-04 13:01:06 -0400

    Add nasamodqbot

---
+++ src/nasamodqbot.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Bot to send new items in modqueue to Discord"""
+
+import logging
+from os import system
+import sys
+
+import praw
+
+from utils import webhook
+from utils.discord_alert import discord_alert
+
+SUB = "TOY"
+
+
+def main():
+    """Main loop"""
+
+    reddit = praw.Reddit("nasamodqbot")
+    app_debug_level = reddit.config.custom['app_debugging'].upper()
+    praw_debug_level = reddit.config.custom['praw_debugging'].upper()
+    logging.basicConfig(level=app_debug_level,
+                        format="%(asctime)s — %(levelname)s - %(funcName)s:%(lineno)d — %(message)s",
+                        datefmt="%c")
+    handler = logging.StreamHandler()
+    handler.setLevel(praw_debug_level)
+    for logger_name in ("praw", "prawcore"):
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(praw_debug_level)
+        logger.addHandler(handler)
+
+    subreddit = reddit.subreddit(SUB)
+    logging.info("Entering main loop")
+    for submission in subreddit.mod.stream.modqueue(skip_existing=True):
+        reddit_url = "https://reddit.com" + submission.permalink
+        logging.debug("New modqueue entry by %s: %s (%s)",
+                      submission.author, submission.title, reddit_url)
+        discord_alert(webhook, "nasamodqbot",
+                      f"New modqueue entry: {submission.title} by {submission.author} {reddit_url}")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as error:
+        system(
+            "ntfy -o priority 1 -t 'nasa modqueue bot crashed' send '" + str(error) + "'")
