fb4e046ec1f332807e8db698f569eba04254ed05
david@kozinn.com <david@kozinn.com>
2022-08-22 20:35:55 -0400

    xpost bot needs to catch/ignore bad link error Fixes #36

--- src/nasaxpost.py
+++ src/nasaxpost.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Bot to crosspost all posts from a specified user"""
-
+# pylint enable=useless-suppression
 import logging
 from os import system
 import sys
@@ -35,10 +35,25 @@ def main():
     redditor = reddit.redditor(USER)
     logging.info("Entering main loop")
     for submission in redditor.stream.submissions(skip_existing=True):
-        cross_post = submission.crosspost(SUB, flair_id="0f2362b2-7fae-11e3-bed4-22000aa47206", send_replies=False)
+        try:
+            cross_post = submission.crosspost(SUB, flair_id="0f2362b2-7fae-11e3-bed4-22000aa47206",
+                                              send_replies=False)
+        except praw.exceptions.RedditAPIException as exception:
+            xpost_error = False
+            for subexception in exception.items:
+                if subexception.error_type == 'INVALID_CROSSPOST_THING':
+                    xpost_error = True
+                    break
+            if xpost_error:
+                logging.warning("Attempt to crosspost http://reddit.com%s failed, skipping",
+                                cross_post.permalink)
+                continue
+            raise
+
         reddit_url = "https://reddit.com" + cross_post.permalink
         logging.info("Cross-posted '%s' from %s at %s",
                      cross_post.title, cross_post.author, reddit_url)
+
 
 if __name__ == "__main__":
     try:
@@ -48,7 +63,7 @@ if __name__ == "__main__":
     except prawcore.exceptions.ServerError:
         logging.exception("Reddit error")
         sys.exit(2)
-    except Exception as error:
+    except Exception as error:  # pylint: disable=broad-except
         logging.exception("Unexpected error")
         system("ntfy -o priority 1 -t 'nasaxpost crashed' send '" + str(error) + "'")
         sys.exit(1)
