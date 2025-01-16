c9b29125e23a86d16462dd89a7ce47ec12104041
David Kozinn <david@kozinn.com>
2025-01-16 00:38:58 +0000

    Fix installation process for database Fixes #57

--- posts.sql
+++ posts.sql
@@ -1,5 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS posts (submission TEXT, rank INTEGER) ;
+CREATE TABLE IF NOT EXISTS posts (submission TEXT, rank INTEGER, ts INTEGER);
 CREATE UNIQUE INDEX IF NOT EXISTS post on posts (submission);
 COMMIT;
